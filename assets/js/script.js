f2c5e9336321546df26fba2c942b965a237c4645
khyan22 <khyanalvarez@gmail.com>
2022-04-07 22:28:26 -0400

    added click event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,35 @@
-// var gameSearch = document.getElementById("game-search");
-// var game;
-
-// var apiUrl = "https://api.rawg.io/api/games?search=" + game + "&key=1162dfcd4c004cc3adeeaa606112ab38";
-
-// var getGameData = function(e) {
-//   e.preventDefault();
-//   game = gameSearch.value;
-
-//   fetch(apiUrl).then(function(response) {
-//     if (response.ok) {
-//       response.json().then(function(response) {
-//         for (i = 0; i < response.results.length; i++) {
-//           console.log(response.results[i]);
-//         }
-//       });
-//     }
-//     else {
-//       console.log("Bad response found");
-//     }
-//   });
-// }
+var searchEl = document.getElementById("game-search-form");
+    var game;
+    var getGameData = function(e) {
+      e.preventDefault();
+      game = searchEl.children[0].value;
+      var apiUrl = "https://api.rawg.io/api/games?search=" + game + "&key=1162dfcd4c004cc3adeeaa606112ab38";
+      fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+          response.json().then(function(response) {
+            // for (i = 0; i < response.results.length; i++) {
+            //   console.log(response.results[i]);
+            // }
+            console.log(response);
+          });
+        }
+        else {
+          console.log("Bad response found");
+        }
+      });
+    }
 
 
+    $("#apiSwitch").on("click", function() {
+        var gameSearchBar = document.getElementById("#")
+        if (searchEl.id === "game-search-form") {
+            $(searchEl).attr("id", "movie-search-form")
+            $("#search").attr("placeholder", "Search Movies and Tv Shows")
+            console.log(searchEl.id)
+        }else if (searchEl.id === "movie-search-form") {
+            $(searchEl).attr("id", "game-search-form")
+            $("#search").attr("placeholder", "Search Games")
+        }
+    })
 
-
-
-
-// gameSearch.addEventlistener("submit", getGameData);
+  searchEl.addEventListener("submit", getGameData);
